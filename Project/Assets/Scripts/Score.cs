af423db4306cc8744f525f0c6a9567d26e78b0d4
WILLIAM VIDA <williamvidaisgod@gmail.com>
2021-04-11 19:32:16 +0100

    Added score system.

---
+++ Project/Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+
+    public float scoreCount;
+    public float pointsPerSeconds;
+    public TextMeshProUGUI scoreText;
+
+    void Start()
+    {
+        scoreText.text = "Score: " + 0000;
+    }
+
+    void Update()
+    {
+        scoreCount += pointsPerSeconds * Time.deltaTime;
+        scoreText.text = "Score: " + scoreCount.ToString("F0");
+    }
+
+}
